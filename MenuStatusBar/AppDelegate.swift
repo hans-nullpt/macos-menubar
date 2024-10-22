8670bdfadd7ce910a54a068eafd99c12ad404495
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-10-22 15:35:30 +0700

    Initial Commit

---
+++ MenuStatusBar/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MenuStatusBar
+//
+//  Created by Ivan Nur Ilham Syah on 22/10/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
---
+++ MenuStatusBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MenuStatusBar
+//
+//  Created by Ivan Nur Ilham Syah on 22/10/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
